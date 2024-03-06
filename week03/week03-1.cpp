e154bddb72fd817083b0ab698a3a4fd60ba998fd
ptkp3t <12751045@me.mcu.edu.tw>
2024-03-06 11:55:05 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("請輸入2個數字");
+    int a, b;
+    scanf("%d", &a); ///讀入a
+    scanf("%d", &b); ///讀入b
+    printf("%5d\n", b);
+    printf("%5d\n", b);
+    printf("---------\n");
+    printf("%5d\n", a+b);
+}
