780113855fffeeb017eb26f9059d1b2258469f90
ptkp3t <12751045@me.mcu.edu.tw>
2024-05-15 11:50:31 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,24 @@
+int main()
+{
+	int a[10][10],b[10][10],c[10][10];
+	int i,j,k,n;
+	//yourcode
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d", &a[i][j] );
+		}
+	}
+for(int i=0; i<n; i++){
+	for(int j=0; j<n;j++){
+		scanf("%d", &b[i][j] );
+	}
+}
+
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			print("%d",a[i][j] );
+		}
+		printf("\n");
+	}
+}
